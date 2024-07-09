89c79e59124e2cd20fbf3a664219776ae5a5df66
Christian Hardy <ChristianTH.to@gmail.com>
2024-07-09 02:48:36 -0400

    Update logreg.py

--- logreg.py
+++ logreg.py
@@ -1,8 +1,8 @@
-# Stdlib
+# Stdlib dependencies
 import warnings
 warnings.filterwarnings('ignore')
 
-# Third party
+# Third party dependencies
 import pandas as pd
 from pandas.plotting import parallel_coordinates
 import numpy as np 
@@ -15,12 +15,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-# Tanh activation function 
+# Hyperbolic tangent activation function 
 def tanh(z):
 	return np.sinh(z)/np.cosh(z)
 
 
-# Regularized loss function
+# Regularized loss function with L1 norm
 def L1_norm_loss_function(theta, X, y, _lambda = 0.1):
 	
 	e = y.size
@@ -30,7 +30,7 @@ def L1_norm_loss_function(theta, X, y, _lambda = 0.1):
 	return (1/e) * (-y.T.dot(np.log(h)) - (1 - y).T.dot(np.log(1 - h))) + reg
 
 
-# Regularized gradient function
+# Regularized gradient function for L1 norm
 def L1_norm_gradient(theta, X, y, _lambda = 0.1):
 	
 	e, f = X.shape
@@ -42,91 +42,81 @@ def L1_norm_gradient(theta, X, y, _lambda = 0.1):
 	return ((1 / e) * X.T.dot(h - y)) + reg
 
 
+# Find the optimal theta co-efficient using logistic regression
+def log_ reg_optimal_theta(X, y, theta):
+    result = opt.minimize(fun = L1_norm_loss_function, x0 = theta, args = (X,y),
+                          method = 'TNC', jac = L1_norm_gradient)
+    return result.x
+
+
 # Import data
 purch_hist = pd.read_csv('purchase_history.csv')
 data = pd.read_csv('data.csv')
 data.head()
 
-
-''' Visualizing the data. Data can clearly be separated by a non-linear solution.
-   a rescaled logistic sigmoid function may be the best solution for now. '''
+''' Visualizing the data to understand the distribution and separability. 
+    Data can be separated by a non-linear solution. A rescaled 
+    logistic sigmoid function would be the obvious solution.'''
 df = data
 sns.regplot(x = 'Age', y = 'Purchased', data = df)
+
 # Output of the data is not linearly seperable 
 plt.show()
-
 age = sns.FacetGrid(data, hue = 'Purchased', size = 5,
                     palette = 'Blues_r').map(plt.scatter,
                                              'EstimatedSalary', 'Age')
 plt.legend(loc = 'lower_right');
 plt.show
 
-
-# Relevant correlation between the features and the output
+# Plot parallel coordinates to see relevant correlations between the features and the output
 parallel_coordinates(data, class_column = 'User ID', cols = ['Age', 'Purchased', 'EstimatedSalary'])
 plt.gca().legend_.remove()
 plt.show()
 
-
 # Data preprocessing
-Purchased = [0, 1] # Output
-e = purch_hist.shape[0] # Examples
-f = 2 # Features
-k = 2 # Classes
+Purchased = [0, 1] # Define output classes
+e = purch_hist.shape[0] # Number of examples
+f = 2 # # Number of features
+k = 2
 
-# Extracting features from the dataset
+# Extract and preprocess features from the dataset
 X = np.ones((e, f + 1))
 y = np.array((e, 1))
 X[:, 1] = purch_hist['Age'].values
 X[:, 2] = purch_hist['EstimatedSalary'].values
+
 # y label
 y = purch_hist['Purchased'].values
 
-
-# Mean normalization
+# Mean normalization of features
 for j in range(f):
-	
 	X[:, j] = (X[:, j] - X[:, j].mean())
 	
-	
-# split data into train & test sets
+# Split data into train & test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 11)
-
  
-''' The hyperbolic tangent activation function is sigmoidal in shape,
-0 is more sharply defined and is more efficient than the logistic 
-function because larger derivatives means faster minimization of
-the loss function. '''
+'''Plot hyperbolic tangent function. The tanh activation function is sigmoidal in shape,
+   0 is more sharply defined and is more efficient than the logistic function because 
+   larger derivatives means faster minimization of the loss function.'''
 x = np.arange(-5,5,0.01)
 y = (2/(1 + np.exp(-2*x))) - 1
 plt.plot(x, y)
 plt.show()
 
-
-# The initial theta co-efficient assumptions of logistic regression 
-def log_ reg_optimal_theta(X, y, theta):
-	
-    result = opt.minimize(fun = L1_norm_loss_function, x0 = theta, args = (X,y),
-                          method = 'TNC', jac = L1_norm_gradient)
-
-    return result.x
-
-
+# Initialize theta for all classes
 all_theta = np.zeros((k, f + 1))
 i = 0
 
-
-# Training 
+# Train the model for each class 
 for purchase in Purchased:
-	
 	tmp_y = np.array(y_train == purchase, dtype = int)
 	optTheta = log_ reg_optimal_theta(X_train, tmp_y, np.zeros((f + 1, 1)))
 	all_theta[i] = optTheta
 	i += 1
 
-
-# Predictions
+# Make predictions on the test set
 P = tanh(X_test.dot(all_theta.T)) # Probability for each customers purchase decision
 P = [Purchased[np.argmax(P[i, :])] for i in range(X_test.shape[0])]
-# Final accuracy
+
+# Calculate and print the accuracy of the model
 print('Test Accuracy', accuracy_score(y_test, P) * 140, '%')
